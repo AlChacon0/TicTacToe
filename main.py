72a1f2ce5e2302a709d7f78aed2ea04f8d74c640
AlChacon0 <alan-chacon@hotmail.com>
2022-12-16 21:29:07 -0700

    MostrarTablero2

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #Crear tablero de juego
 tablero = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
 ]
 
 #Mostrar tablero
-print(' ' + tablero[0][0] + ' | ' + tablero[0][1] + ' | ' + tablero[0][2]) 
-print('---+---+---')
-print(' ' + tablero[1][0] + ' | ' + tablero[1][1] + ' | ' + tablero[1][2]) 
-print('---+---+---')
-print(' ' + tablero[2][0] + ' | ' + tablero[2][1] + ' | ' + tablero[2][2]) 
+fila = 0
+columna = 0
+for lista in tablero:
+    while columna < 3:
+        print(' ' + tablero[fila][columna] + ' |', end='')
+        columna += 1
+    print("")
+    if fila < 2: print('---+---+---|')
+    fila += 1
+    columna = 0

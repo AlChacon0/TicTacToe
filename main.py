f426c99da193d7ff2d840095c3f0da142a633cdd
AlChacon0 <alan-chacon@hotmail.com>
2022-12-16 21:17:41 -0700

    MostrarTablero

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #Crear tablero de juego
+tablero = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+#Mostrar tablero
+print(' ' + tablero[0][0] + ' | ' + tablero[0][1] + ' | ' + tablero[0][2]) 
+print('---+---+---')
+print(' ' + tablero[1][0] + ' | ' + tablero[1][1] + ' | ' + tablero[1][2]) 
+print('---+---+---')
+print(' ' + tablero[2][0] + ' | ' + tablero[2][1] + ' | ' + tablero[2][2]) 
